d8b747970633a81e764cde8c6ef4f38a3e6e9acb
Hunter Metts <huntermettsdev@gmail.com>
2019-01-22 12:01:01 -0600

    challenge complete

--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -1,11 +1,21 @@
 const remoteURL = "http://localhost:5002"
 
 export default {
-    // Animal fetches
+    // Animal fetches to populate page
   getAnimal(id) {
     return fetch(`${remoteURL}/animals/${id}`).then(e => e.json())
   },
   getAllAnimals() {
     return fetch(`${remoteURL}/animals`).then(e => e.json())
+  },
+
+  // Delete module, chained to application views
+  removeAndList(id){
+          return fetch(`http://localhost:5002/animals/${id}`, {
+              method: "DELETE"
+          })
+          .then(e => e.json())
+          .then(() => fetch(`http://localhost:5002/animals`))
+          .then(e => e.json())
   }
 }
